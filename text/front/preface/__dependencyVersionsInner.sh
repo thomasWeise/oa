378daa06dbc6a76f0e954f3507f23de713470766
Thomas Weise <tweise@hfuu.edu.cn>
2026-02-22 15:53:59 +0800

    yet another attempt 5

--- text/front/preface/__dependencyVersionsInner.sh
+++ text/front/preface/__dependencyVersionsInner.sh
@@ -15,8 +15,8 @@ hasOutput=false  # Do we have some output and need a separator?
 packages=("moptipy")
 
 # python3 -m pip install --require-virtualenv "${packages[@]}" 1>/dev/null 2>&1
-python3 -m pip install -v --require-virtualenv "${packages[@]}" --log out_1.txt 1>out_2.txt 2>out_3.txt || true
-for f in "out_1.txt" "out_2.txt" "out_3.txt"; do
+python3 -m pip install --require-virtualenv "${packages[@]}" --log out_1.txt 1>out_2.txt 2>out_3.txt || true
+for f in "out_1.txt"; do
 sed -r \
 -e 's/\x00/[NUL]/g' \
 -e 's/\x01/[SOH]/g' \
