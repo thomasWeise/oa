0f95f2b1bb7357adeb23f7d91014c57145a0e43f
Thomas Weise <tweise@hfuu.edu.cn>
2026-02-22 14:13:10 +0800

    yet another attempt 2

--- text/front/preface/__dependencyVersionsInner.sh
+++ text/front/preface/__dependencyVersionsInner.sh
@@ -13,10 +13,45 @@ hasOutput=false  # Do we have some output and need a separator?
 
 # Make sure that all packages are installed.
 packages=("moptipy")
+
 # python3 -m pip install --require-virtualenv "${packages[@]}" 1>/dev/null 2>&1
 python3 -m pip install --require-virtualenv "${packages[@]}" 1>out_1.txt 2>&1 || true
-iconv -f UTF-8 -t ASCII//TRANSLIT out_1.txt -o out_2.txt || true
-cat out_2.txt || true
+sed -r \
+-e 's/\x00/[NUL]/g' \
+-e 's/\x01/[SOH]/g' \
+-e 's/\x02/[STX]/g' \
+-e 's/\x03/[ETX]/g' \
+-e 's/\x04/[EOT]/g' \
+-e 's/\x05/[ENQ]/g' \
+-e 's/\x06/[ACK]/g' \
+-e 's/\x07/[BEL]/g' \
+-e 's/\x08/[BS]/g' \
+-e 's/\x09/[HT]/g' \
+-e 's/\x0A/[LF]/g' \
+-e 's/\x0B/[VT]/g' \
+-e 's/\x0C/[FF]/g' \
+-e 's/\x0D/[CR]/g' \
+-e 's/\x0E/[SO]/g' \
+-e 's/\x0F/[SI]/g' \
+-e 's/\x10/[DLE]/g' \
+-e 's/\x11/[DC1]/g' \
+-e 's/\x12/[DC2]/g' \
+-e 's/\x13/[DC3]/g' \
+-e 's/\x14/[DC4]/g' \
+-e 's/\x15/[NAK]/g' \
+-e 's/\x16/[SYN]/g' \
+-e 's/\x17/[ETB]/g' \
+-e 's/\x18/[CAN]/g' \
+-e 's/\x19/[EM]/g' \
+-e 's/\x1A/[SUB]/g' \
+-e 's/\x1B/[ESC]/g' \
+-e 's/\x1C/[FS]/g' \
+-e 's/\x1D/[GS]/g' \
+-e 's/\x1E/[RS]/g' \
+-e 's/\x1F/[US]/g' \
+-e 's/\x7F/[DEL]/g' out_1.txt > out_2.txt
+sed 's/[^\x00-\x7F]//g' out_2.txt > out_3.txt
+cat out_3.txt
 exit 0
 # Check the versions of the tools and packages.
 for pack in "${packages[@]}"; do
